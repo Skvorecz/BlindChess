1d527c10c43247fd9e960e7b96599017e2c8f6e7
Andrey_Skvorcov <Skvorec-2010@mail.ru>
2021-12-09 23:46:21 +0300

    Rook added

---
+++ Core/Rook.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Core
+{
+    public class Rook : Piece
+    {
+        public Rook(IBoard board) : base(board)
+        {
+        }
+
+        public override List<Square> GetPossibleMoves()
+        {
+            var possibleMoves = new List<Square>();
+
+            possibleMoves.AddRange(Board.GetOtherSquaresOnSameFile(Position));
+            possibleMoves.AddRange(Board.GetOtherSquaresOnSameRank(Position));
+
+            return possibleMoves;
+        }
+    }
+}
