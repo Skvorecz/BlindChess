5589e322151342dc386ad7a7a7aa9487e70d2731
Andrey_Skvorcov <Skvorec-2010@mail.ru>
2021-12-09 23:42:53 +0300

    Board added with implementation

---
+++ Core/Board.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+
+namespace Core
+{
+    public class Board : IBoard
+    {
+        private const File LeftFile = File.a;
+        private const File RightFile = File.h;
+        private const Rank BottomRank = Rank.One;
+        private const Rank TopRank = Rank.Eight;
+
+        public bool IsThereSquareOnTheLeft(Square square) => square.File != LeftFile;
+        public bool IsThereSquareOnTheRight(Square square) => square.File != RightFile;
+        public bool IsThereSquareBelow(Square square) => square.Rank != BottomRank;
+        public bool IsThereSquareAbove(Square square) => square.Rank != TopRank;
+
+        public bool IsThereSquareOnTheLeftAndBelow(Square square) => IsThereSquareOnTheLeft(square) && IsThereSquareBelow(square);
+        public bool IsThereSquareOnTheRightAndBelow(Square square) => IsThereSquareOnTheRight(square) && IsThereSquareBelow(square);
+        public bool IsThereSquareOnTheLeftAndAbove(Square square) => IsThereSquareOnTheLeft(square) && IsThereSquareAbove(square);
+        public bool IsThereSquareOnTheRightAndAbove(Square square) => IsThereSquareOnTheRight(square) && IsThereSquareAbove(square);
+
+        public Square GetSquareOnTheLeft(Square square) => new Square(square.File - 1, square.Rank);
+        public Square GetSquareOnTheRight(Square square) => new Square(square.File + 1, square.Rank);
+        public Square GetSquareBelow(Square square) => new Square(square.File, square.Rank - 1);
+        public Square GetSquareAbove(Square square) => new Square(square.File, square.Rank + 1);
+
+        public Square GetSquareOnTheLeftAndBelow(Square square) => new Square(square.File - 1, square.Rank - 1);
+        public Square GetSquareOnTheRightAndBelow(Square square) => new Square(square.File + 1, square.Rank - 1);
+        public Square GetSquareOnTheLeftAndAbove(Square square) => new Square(square.File - 1, square.Rank + 1);
+        public Square GetSquareOnTheRightAndAbove(Square square) => new Square(square.File + 1, square.Rank + 1);
+
+        public List<Square> GetOtherSquaresOnSameFile(Square square)
+        {
+            var squares = new List<Square>();
+            var currentSquare = square;
+            while (IsThereSquareBelow(currentSquare))
+            {
+                currentSquare = GetSquareBelow(square);
+                squares.Add(currentSquare);
+            }
+
+            currentSquare = square;
+            while (IsThereSquareAbove(currentSquare))
+            {
+                currentSquare = GetSquareAbove(square);
+                squares.Add(currentSquare);
+            }
+
+            return squares;
+        }
+
+        public List<Square> GetOtherSquaresOnSameRank(Square square)
+        {
+            var squares = new List<Square>();
+            var currentSquare = square;
+            while (IsThereSquareOnTheLeft(currentSquare))
+            {
+                currentSquare = GetSquareOnTheLeft(square);
+                squares.Add(currentSquare);
+            }
+
+            currentSquare = square;
+            while (IsThereSquareOnTheRight(currentSquare))
+            {
+                currentSquare = GetSquareOnTheRight(square);
+                squares.Add(currentSquare);
+            }
+
+            return squares;
+        }
+    }
+}
---
+++ Core/IBoard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Core
+{
+    public interface IBoard
+    {
+        List<Square> GetOtherSquaresOnSameFile(Square square);
+        List<Square> GetOtherSquaresOnSameRank(Square square);
+        Square GetSquareAbove(Square square);
+        Square GetSquareBelow(Square square);
+        Square GetSquareOnTheLeft(Square square);
+        Square GetSquareOnTheLeftAndAbove(Square square);
+        Square GetSquareOnTheLeftAndBelow(Square square);
+        Square GetSquareOnTheRight(Square square);
+        Square GetSquareOnTheRightAndAbove(Square square);
+        Square GetSquareOnTheRightAndBelow(Square square);
+        bool IsThereSquareAbove(Square square);
+        bool IsThereSquareBelow(Square square);
+        bool IsThereSquareOnTheLeft(Square square);
+        bool IsThereSquareOnTheLeftAndAbove(Square square);
+        bool IsThereSquareOnTheLeftAndBelow(Square square);
+        bool IsThereSquareOnTheRight(Square square);
+        bool IsThereSquareOnTheRightAndAbove(Square square);
+        bool IsThereSquareOnTheRightAndBelow(Square square);
+    }
+}
