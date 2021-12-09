b435353394e7e385ed644ce53737ddca70da0d90
Andrey_Skvorcov <Skvorec-2010@mail.ru>
2021-12-09 21:59:51 +0300

    Add project files.

---
+++ BlindChessConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlindChessConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var serviceProvider = ConfigureServices();
+            serviceProvider.GetRequiredService<Application>().Run();
+        }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton<Application>();
+            return services.BuildServiceProvider();
+        }
+    }
+}
